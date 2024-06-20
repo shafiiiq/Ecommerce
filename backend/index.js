930fdfa1dcb82cc03e76685c157b37bb123193f1
Muhammed Shafeek <shafiiq688@gmail.com>
2024-06-20 23:42:48 +0530

    intialy i uploaded my project into shafiiiq/E-commerce reponsitory then i get error while fetching the code so i created new one pushed fixed code to this repo now all fixed an can retrive and changed something newfor structured code

--- backend/index.js
+++ backend/index.js
@@ -287,6 +287,7 @@ app.post('/login', async (req, res) => {
         email: req.body.email
     })
 
+    // if user exists then user can login 
     if (user) {
         const comparePswd = req.body.password === user.password;
 
@@ -318,10 +319,13 @@ app.post('/login', async (req, res) => {
 //Creating a middleware to authenticate user
 const authUser = (req, res, next) => {
     const token = req.header('auth-token');
+
+    // if user has no token it will be retuned 
     if (!token) {
         return res.status(401).send({ errors: "Please authenticate using a valid token" });
     }
 
+    // if user has token then verify the token 
     try {
         const data = jwt.verify(token, 'secret_ecom');
         req.user = data.user;
